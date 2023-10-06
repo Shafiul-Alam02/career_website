250109614b2246254173d2f814d867b9cfc4f2af
Shafiul-Alam02 <63425008+Shafiul-Alam02@users.noreply.github.com>
2023-10-06 19:53:10 +0000

    decoration added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-  return "hello"
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
