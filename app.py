fc72a08be7f87a9ba63a26c6f92d98abc718ff44
Shafiul-Alam02 <63425008+Shafiul-Alam02@users.noreply.github.com>
2023-10-08 20:52:26 +0000

    modified

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+JOBS=[
+  {'id':1, 
+  'title': 'Software developer',
+'experience':'2 years'}
+]
+
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, person="Shafiul Alam")
 
 
 if __name__ == '__main__':
